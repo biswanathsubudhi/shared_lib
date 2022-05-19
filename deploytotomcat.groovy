d628fa0be7d956a803be00b5ed7b8a1563d87178
biswanathsubudhi <100853276+biswanathsubudhi@users.noreply.github.com>
2022-05-19 21:48:23 +0530

    Delete deploytotomcat.groovy

--- deploytotomcat.groovy
+++
@@ -1,7 +0,0 @@
-def call(envt,user_name,ip) {
-  sshagent(['envt']) {
-       sh 'scp -o StrictHostKeyChecking=no target/myweb.war $user_name@$ip:/software/tomcat-9/webapps/'
-       sh 'ssh $user_name@$ip /software/tomcat-9/bin/shutdown.sh'
-       sh 'ssh $user_name@$ip /software/tomcat-9/bin/startup.sh'
-      }
-   }
