6df6cede13352909c4c6c2c9fece792226ea43a0
biswanathsubudhi <100853276+biswanathsubudhi@users.noreply.github.com>
2022-05-19 21:26:50 +0530

    Create deploytotomcat.groovy

---
+++ deploytotomcat.groovy
@@ -0,0 +1,7 @@
+def call(env,user_name,ip) {
+  sshagent(['envt']) {
+       sh 'scp -o StrictHostKeyChecking=no target/myweb.war $user_name@$ip:/software/tomcat-9/webapps/'
+       sh 'ssh $user_name@$ip /software/tomcat-9/bin/shutdown.sh'
+       sh 'ssh $user_name@$ip /software/tomcat-9/bin/startup.sh'
+      }
+   }
